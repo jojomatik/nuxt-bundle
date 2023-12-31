0151aaffcb136dbc89d173eb3b7cf325983c6f35
jojomatik <dev@jojomatik.de>
2023-12-31 13:18:04 +0100

    ci: add CI and CD with semantic-release

---
+++ release.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [{ type: "build", scope: "deps", release: "patch" }],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            {
+              type: "feat",
+              section: "Features",
+              hidden: false,
+            },
+            {
+              type: "fix",
+              section: "Bug Fixes",
+              hidden: false,
+            },
+            {
+              type: "perf",
+              section: "Performance Improvements",
+              hidden: false,
+            },
+            {
+              type: "build",
+              scope: "deps",
+              hidden: false,
+              section: "Dependency updates",
+            },
+          ],
+        },
+      },
+    ],
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: [["**/package*.json", "!node_modules/**"]],
+        // eslint-disable-next-line no-template-curly-in-string
+        message: "release: ${nextRelease.version}",
+      },
+    ],
+  ],
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    { name: "beta", prerelease: true },
+  ],
+};
