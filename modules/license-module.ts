d7aa22b557aa5d9f8249be41529c6593151bb883
jojomatik <dev@jojomatik.de>
2026-01-03 22:31:04 +0100

    fix: simplify license module
    Simplify license module based on new features.

--- modules/license-module.ts
+++ modules/license-module.ts
@@ -1,6 +1,5 @@
-import { defineNuxtModule, useLogger } from "nuxt/kit";
+import { addVitePlugin, defineNuxtModule, useLogger } from "nuxt/kit";
 import license from "rollup-plugin-license";
-import { type PluginOption } from "vite";
 
 const moduleKey = "@jojomatik/nuxt-bundle/license-module";
 const envKey = "ALLOWED_LICENCES";
@@ -12,81 +11,78 @@ export default defineNuxtModule({
     name: moduleKey,
     configKey: "licenseModule",
   },
-  setup(options, nuxt) {
-    nuxt.hook("vite:extendConfig", (viteConfig) => {
-      viteConfig.plugins = viteConfig.plugins || [];
-      viteConfig.plugins.push(
-        license({
-          thirdParty: {
-            includePrivate: false,
-            allow: {
-              test: (dependency) => {
-                // Return false for unlicensed dependencies.
-                if (!dependency.license) return false;
+  setup(options) {
+    addVitePlugin(
+      license({
+        thirdParty: {
+          includePrivate: false,
+          allow: {
+            test: (dependency) => {
+              // Return false for unlicensed dependencies.
+              if (!dependency.license) return false;
 
-                // Load allowed licenses from environment-variable
-                const allowedLicensesFromEnv = process.env[envKey]
-                  ?.split(",")
-                  .map((license) => license.trim());
+              // Load allowed licenses from environment-variable
+              const allowedLicensesFromEnv = process.env[envKey]
+                ?.split(",")
+                .map((license) => license.trim());
 
-                // Load allowed licenses from module options
-                // Allow MIT and Apache-2.0 licenses by default
-                const allowedLicenses =
-                  allowedLicensesFromEnv ?? options.allowedLicenses;
+              // Load allowed licenses from module options
+              // Allow MIT and Apache-2.0 licenses by default
+              const allowedLicenses =
+                allowedLicensesFromEnv ?? options.allowedLicenses;
 
-                const isLicenseAllowed = allowedLicenses.includes(
-                  dependency.license,
+              const isLicenseAllowed = allowedLicenses.includes(
+                dependency.license,
+              );
+
+              if (!isLicenseAllowed) {
+                logger.error(
+                  `Dependency "${dependency.name}" has a license (${dependency.license}) that is not the list of allowed licenses (${allowedLicenses.join(", ")}).`,
                 );
 
-                if (!isLicenseAllowed) {
-                  logger.error(
-                    `Dependency "${dependency.name}" has a license (${dependency.license}) that is not the list of allowed licenses (${allowedLicenses.join(", ")}).`,
-                  );
+                const requiredLicenses = [
+                  ...allowedLicenses,
+                  dependency.license,
+                ];
 
-                  const requiredLicenses = [
-                    ...allowedLicenses,
-                    dependency.license,
-                  ];
-
-                  const configContent = JSON.stringify(
-                    {
-                      extends: "@jojomatik/nuxt-bundle",
-                      licenseModule: {
-                        allowedLicenses: requiredLicenses,
-                      },
+                const configContent = JSON.stringify(
+                  {
+                    extends: "@jojomatik/nuxt-bundle",
+                    licenseModule: {
+                      allowedLicenses: requiredLicenses,
                     },
-                    undefined,
-                    2,
-                  );
+                  },
+                  undefined,
+                  2,
+                );
 
-                  const configExample =
-                    "export default defineNuxtConfig(" + configContent + ");\n";
+                const configExample =
+                  "export default defineNuxtConfig(" + configContent + ");\n";
 
-                  logger.info(
-                    `You can specify allowed licenses in "nuxt.config.ts" environment variable:`,
-                  );
-                  console.log(configExample);
+                logger.info(
+                  `You can specify allowed licenses in "nuxt.config.ts" environment variable:`,
+                );
+                console.log(configExample);
 
-                  logger.info(
-                    `You can also specify allowed licenses via "${envKey}" environment variable (ALLOWED_LICENCES=${requiredLicenses.join(",")}).`,
-                  );
-                }
+                logger.info(
+                  `You can also specify allowed licenses via "${envKey}" environment variable (ALLOWED_LICENCES=${requiredLicenses.join(",")}).`,
+                );
+              }
 
-                return isLicenseAllowed;
-              },
-              failOnUnlicensed: true,
-              failOnViolation: true,
+              return isLicenseAllowed;
             },
-            output: {
-              // Output file into public directory which is included in the build output.
-              file: "public/dependencies.json",
-              template(dependencies) {
-                return JSON.stringify(dependencies);
-              },
+            failOnUnlicensed: true,
+            failOnViolation: true,
+          },
+          output: {
+            // Output file into public directory which is included in the build output.
+            file: "public/dependencies.json",
+            template(dependencies) {
+              return JSON.stringify(dependencies);
             },
           },
-        }) as PluginOption,
-      );
-    });
+        },
+      }),
+    );
   },
 });
