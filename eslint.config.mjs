9cc5bc62f2538119d617a1bfd8e25018dab8dea3
jojomatik <dev@jojomatik.de>
2026-01-03 20:05:07 +0100

    build: migrate eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import storybook from "eslint-plugin-storybook";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default withNuxt([
+  eslintPluginPrettierRecommended,
+  ...storybook.configs["flat/recommended"],
+  { ignores: ["!.storybook"] },
+  {
+    rules: {
+      "no-console":
+        process.env.NODE_ENV === "production"
+          ? [
+              "error",
+              {
+                allow: ["warn", "error"],
+              },
+            ]
+          : "off",
+
+      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    },
+  },
+  {
+    files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+  },
+]);
